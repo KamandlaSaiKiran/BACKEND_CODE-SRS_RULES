6e90e0ef79b49af9a9601110099355b4c1bb3271
unknown <kamandlasaikiran@gmail.com>
2025-06-04 21:28:58 +0530

    Final one

--- server.js
+++ server.js
@@ -7,7 +7,24 @@ const NodeCache = require('node-cache');
 const cache = new NodeCache({ stdTTL: 3600, checkperiod: 120 });
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+
+const allowedOrigins = [
+  'https://frontend-code-srs-rules.vercel.app',
+  'http://localhost:5173'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.post('/rule', async (req, res) => {
