00b282f6cb965fc9f18f702032cf9a2f4791c210
unknown <kamandlasaikiran@gmail.com>
2025-06-04 21:40:56 +0530

    Final two

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post('/rule', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Backend server is running on port ${PORT}`);
 });
